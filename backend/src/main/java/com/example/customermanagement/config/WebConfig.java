be98c466e6c5fc2c2df6462f13505090e4fb383b
Sreejay1804 <opensreejay@gmail.com>
2025-07-02 18:54:28 +0530

    Deployed Successfully

--- backend/src/main/java/com/example/customermanagement/config/WebConfig.java
+++ backend/src/main/java/com/example/customermanagement/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000", "https://ecomwebapp1.netlify.app")
+                .allowedOrigins("http://localhost:3000", "https://ecommercebts.netlify.app/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
